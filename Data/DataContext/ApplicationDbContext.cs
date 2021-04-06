a852f73a1c54fde4d73121a751663b69e5c294a3
Statkeivch Roman <romanstat@mail.ru>
2021-04-06 13:02:54 +0300

    Added ApplicationDbContext

---
+++ Data/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Vendor> Vendors { get; set; }
+
+        public DbSet<Producer> Producers { get; set; }
+
+        public DbSet<Autopart> Autoparts { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
