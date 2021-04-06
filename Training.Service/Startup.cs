9435fb1ead6dc80d8ee7d6d3d41c8c966693876a
Statkeivch Roman <romanstat@mail.ru>
2021-04-06 14:17:27 +0300

    renaming

--- Training.Service/Startup.cs
+++ Training.Service/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
-namespace Training
+namespace Training.Service
 {
     public class Startup
     {
