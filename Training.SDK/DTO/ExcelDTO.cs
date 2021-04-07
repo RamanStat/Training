ad0f96cb7f5cf7818e2fdd86cc806e427fe386bb
Statkeivch Roman <romanstat@mail.ru>
2021-04-07 12:01:09 +0300

    Added ExcelDTO

---
+++ Training.SDK/DTO/ExcelDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace Training.SDK.DTO
+{
+    public class ExcelDTO
+    {
+        public string AutopartName { get; set; }
+
+        public double AutopartPrice { get; set; }
+
+        public string AutopartDescription { get; set; }
+
+        public string ProducerName { get; set; }
+
+        public string CarModel { get; set; }
+
+        public string CarIssueYear { get; set; }
+
+        public string CarEngine { get; set; }
+
+        public string VendorName { get; set; }
+    }
+}
