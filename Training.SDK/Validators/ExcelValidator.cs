bc17d6c51c7ec543f175430ad588809b3ceee241
Statkeivch Roman <romanstat@mail.ru>
2021-04-07 18:27:29 +0300

    Added test endpoint for excel

---
+++ Training.SDK/Validators/ExcelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Training.SDK.DTO;
+
+namespace Training.SDK.Validators
+{
+    class ExcelValidator : AbstractValidator<ExcelDTO>
+    {
+        public ExcelValidator()
+        {
+            RuleFor(e => e.AutopartName).NotNull();
+        }
+    }
+}
--- Training.Service/Controllers/TestController.cs
+++ Training.Service/Controllers/TestController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Training.RA.Interfaces;
+using Training.SDK.DTO;
 
 namespace Training.Service.Controllers
 {
@@ -16,9 +21,39 @@ namespace Training.Service.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAutopart()
+        public async Task<IActionResult> GetAutopartAsync()
         {
             return Ok(await _autopartRepository.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetExcel(IFormFile file)
+        {
+            var excelDTOs = new List<ExcelDTO>();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Create(file.FileName))
+            {
+                await file.CopyToAsync(stream);
+                using var reader = ExcelReaderFactory.CreateReader(stream);
+                reader.Read();
+
+                while (reader.Read())
+                {
+                    excelDTOs.Add(new ExcelDTO()
+                    {
+                        AutopartName = reader.GetValue(0).ToString(),
+                        AutopartPrice = int.Parse(reader.GetValue(1).ToString()),
+                        AutopartDescription = reader.GetValue(2).ToString(),
+                        ProducerName = reader.GetValue(3).ToString(),
+                        CarModel = reader.GetValue(4).ToString(),
+                        CarIssueYear = int.Parse(reader.GetValue(5).ToString()),
+                        CarEngine = int.Parse(reader.GetValue(6).ToString()),
+                        VendorName = reader.GetValue(7).ToString(),
+                    });
+                }
+            }
+
+            return Ok(excelDTOs);
         }
     }
 }
