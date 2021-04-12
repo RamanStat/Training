e537c70eb16df0d6428a82fad15f2b2e6c447cff
Statkeivch Roman <romanstat@mail.ru>
2021-04-12 14:57:53 +0300

    Implemented ProducerService

---
+++ Training.SDK/Interfaces/IProducerService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Training.SDK.DTO;
+
+namespace Training.SDK.Interfaces
+{
+    public interface IProducerService
+    {
+        Task<IEnumerable<ProducerDTO>> GetProducersAsync();
+
+        Task<ProducerDTO> CreateProducerAsync(ProducerDTO producerDTO);
+
+        Task<ProducerDTO> GetProducerAsync(int id);
+
+        Task<ProducerDTO> UpdateProducerAsync(ProducerDTO producerDTO);
+
+        Task<ProducerDTO> DeleteProducerAsync(int id);
+    }
+}
---
+++ Training.SDK/Services/ProducerService.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Training.Data.Entities;
+using Training.RA.Interfaces;
+using Training.SDK.DTO;
+using Training.SDK.Interfaces;
+
+namespace Training.SDK.Services
+{
+    public class ProducerService : IProducerService
+    {
+        private readonly IMapper _mapper;
+        private readonly IProducerRepository _producerRepository;
+
+        public ProducerService(IMapper mapper, IProducerRepository producerRepository)
+        {
+            _mapper = mapper;
+            _producerRepository = producerRepository;
+        }
+
+        public async Task<ProducerDTO> GetProducerAsync(int id)
+        {
+            var producer = await _producerRepository.GetByIdAsync(id);
+
+            return _mapper.Map<ProducerDTO>(producer);
+        }
+
+        public async Task<IEnumerable<ProducerDTO>> GetProducersAsync()
+        {
+            var producers = await _producerRepository.GetAllAsync();
+
+            return _mapper.Map<ProducerDTO[]>(producers);
+        }
+
+        public async Task<ProducerDTO> CreateProducerAsync(ProducerDTO producerDTO)
+        {
+            var producer = _mapper.Map<Producer>(producerDTO);
+
+            producer = await _producerRepository.CreateAsync(producer);
+
+            return _mapper.Map<ProducerDTO>(producer);
+        }
+
+        public async Task<ProducerDTO> UpdateProducerAsync(ProducerDTO producerDTO)
+        {
+            var producer = _mapper.Map<Producer>(producerDTO);
+
+            await _producerRepository.UpdateAsync(producer);
+
+            return producerDTO;
+        }
+
+        public async Task<ProducerDTO> DeleteProducerAsync(int id)
+        {
+            var producer = await _producerRepository.GetByIdAsync(id);
+
+            await _producerRepository.DeleteAsync(producer);
+
+            return _mapper.Map<ProducerDTO>(producer);
+        }
+    }
+}
