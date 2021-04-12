27997d16eba24d1c5ff026816569e9cce19cf554
Statkeivch Roman <romanstat@mail.ru>
2021-04-12 14:57:02 +0300

    Added ProducerDTO

---
+++ Training.SDK/DTO/ProducerDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Training.SDK.DTO
+{
+    public class ProducerDTO
+    {
+        public int Id { get; set; } 
+        
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
