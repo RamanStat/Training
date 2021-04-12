6c7f18a828dbb8a01e4359ade8204e6949c0130f
Statkeivch Roman <romanstat@mail.ru>
2021-04-12 14:58:59 +0300

    Added UnitTests for ProducerService

---
+++ Training.Tests/ProducerFixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Training.Data.Entities;
+
+namespace Training.Tests
+{
+    public class ProducerFixture
+    {
+        private readonly List<Producer> _producers;
+
+        public ProducerFixture()
+        {
+            _producers = new List<Producer>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Name = "test",
+                    Address = "test",
+                    Phone = "test"
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "test",
+                    Address = "test",
+                    Phone = "test"
+                }
+            };
+        }
+        
+        public Producer GetProducer(int id)
+        {
+            return _producers[id];
+        }
+
+        public IEnumerable<Producer> GetProducers()
+        {
+            return _producers;
+        }
+    }
+}
---
+++ Training.Tests/Services/ProducerServiceTest.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using Training.Data.Entities;
+using Training.RA.Interfaces;
+using Training.SDK.DTO;
+using Training.SDK.EqualityComparers;
+using Training.SDK.Mapping;
+using Training.SDK.Services;
+
+namespace Training.Tests.Services
+{
+    public class ProducerServiceTest
+    {
+        private readonly List<ProducerDTO> _producers;
+        private readonly IMapper _mapper;
+        private readonly Mock<IProducerRepository> _mockProducerRepository;
+        private readonly ProducerFixture _producerFixture;
+
+        public ProducerServiceTest()
+        {
+            _producers = new List<ProducerDTO>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Address = "test",
+                    Name = "test",
+                    Phone = "test"
+                },
+                new()
+                {
+                    Id = 2,
+                    Address = "test",
+                    Name = "test",
+                    Phone = "test"
+                }
+            };
+            
+            _producerFixture = new ProducerFixture();
+            _mapper = new MapperConfiguration(mc => mc.AddProfile(new MappingProfile())).CreateMapper();
+
+            _mockProducerRepository = new Mock<IProducerRepository>();
+
+            _mockProducerRepository.Setup(pr => pr.GetAllAsync(It.IsAny<CancellationToken>()))
+                .Returns(Task.Run(() => _producerFixture.GetProducers()));
+            _mockProducerRepository.Setup(pr => pr.GetByIdAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.Run(() => _producerFixture.GetProducer(It.IsAny<int>())));
+            _mockProducerRepository.Setup(pr => pr.CreateAsync(It.IsAny<Producer>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.Run(() => _producerFixture.GetProducer(It.IsAny<int>())));
+            _mockProducerRepository.Setup(pr => pr.UpdateAsync(It.IsAny<Producer>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.Run(() => _producerFixture.GetProducer(It.IsAny<int>())));
+            _mockProducerRepository.Setup(pr => pr.DeleteAsync(It.IsAny<Producer>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.Run(() => _producerFixture.GetProducer(It.IsAny<int>())));
+        }
+
+        [Test]
+        public async Task GetProducersAsync_InvokeMethod_GettingProducers()
+        {
+            //Arrange
+            var expectedProducers = _producers;
+            var producerService = new ProducerService(_mapper, _mockProducerRepository.Object);
+
+            //Act
+            var producers = await producerService.GetProducersAsync();
+
+            //Assert
+            Assert.That(expectedProducers, Is.EqualTo(producers).Using(new ProducerDTOEquilityComparer()));
+        }
+
+        [Test]
+        public async Task GetProducerAsync_SentId_GettingProducerById()
+        {
+            //Arrange
+            var expectedProducer = _producers[0];
+            var producerService = new ProducerService(_mapper, _mockProducerRepository.Object);
+
+            //Act
+            var producer = await producerService.GetProducerAsync(1);
+
+            //Assert
+            Assert.That(expectedProducer, Is.EqualTo(producer).Using(new ProducerDTOEquilityComparer()));
+        }
+
+        [Test]
+        public async Task CreateProducerAsync_SentRightProducer_GettingCreatedProducer()
+        {
+            //Arrange
+            var expectedProducer = _producers[0];
+            var producerService = new ProducerService(_mapper, _mockProducerRepository.Object);
+
+            //Act
+            var producer = await producerService.CreateProducerAsync(_producers[0]);
+
+            //Assert
+            Assert.That(expectedProducer, Is.EqualTo(producer).Using(new ProducerDTOEquilityComparer()));
+        }
+
+        [Test]
+        public async Task UpdateProducerAsync_SentRightProducer_GettingUpdatedProducer()
+        {
+            //Arrange
+            var expectedProducer = _producers[0];
+            var producerService = new ProducerService(_mapper, _mockProducerRepository.Object);
+
+            //Act
+            var producer = await producerService.UpdateProducerAsync(_producers[0]);
+
+            //Assert
+            Assert.That(expectedProducer, Is.EqualTo(producer).Using(new ProducerDTOEquilityComparer()));
+        }
+
+        [Test]
+        public async Task DeleteProducerAsync_SentId_GettingDeletedProducer()
+        {
+            //Arrange
+            var expectedProducer = _producers[0];
+            var producerService = new ProducerService(_mapper, _mockProducerRepository.Object);
+
+            //Act
+            var producer = await producerService.CreateProducerAsync(_producers[0]);
+
+            //Assert
+            Assert.That(expectedProducer, Is.EqualTo(producer).Using(new ProducerDTOEquilityComparer()));
+        }
+    }
+}
