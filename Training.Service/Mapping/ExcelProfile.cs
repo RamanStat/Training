f88a2deea930342e24cfbb3aec4d7d57990ff69f
Statkeivch Roman <romanstat@mail.ru>
2021-04-12 19:32:16 +0300

    Splited MappingProfile to ExcelProfile and ProducerProfile

---
+++ Training.Service/Mapping/ExcelProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ExcelDataReader;
+using Training.SDK.DTO;
+
+namespace Training.Service.Mapping
+{
+    public class ExcelProfile : Profile
+    {
+        public ExcelProfile()
+        {
+            CreateMap<IExcelDataReader, ExcelDTO>()
+                .ForMember(e => e.AutopartName, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.AutopartName_COLUMN_OFFSET)))
+                .ForMember(e => e.AutopartPrice, source => source.MapFrom(s => double.Parse(s.GetValue(Constants.ImportFileOffsets.AutopartPrice_COLUMN_OFFSET).ToString())))
+                .ForMember(e => e.AutopartDescription, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.AutopartDescription_COLUMN_OFFSET)))
+                .ForMember(e => e.ProducerName, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.Producer_NameCOLUMN_OFFSET)))
+                .ForMember(e => e.CarModel, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.CarModel_COLUMN_OFFSET)))
+                .ForMember(e => e.CarIssueYear, source => source.MapFrom(s => int.Parse(s.GetValue(Constants.ImportFileOffsets.CarIssueYear_COLUMN_OFFSET).ToString())))
+                .ForMember(e => e.CarEngine, source => source.MapFrom(s => int.Parse(s.GetValue(Constants.ImportFileOffsets.CarEngine_COLUMN_OFFSET).ToString())))
+                .ForMember(e => e.VendorName, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.VendorName_COLUMN_OFFSET)))
+                .ReverseMap();
+        }
+    }
+}
---
+++ Training.Service/Mapping/ProducerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ExcelDataReader;
+using Training.Data.Entities;
+using Training.SDK.DTO;
+
+namespace Training.Service.Mapping
+{
+    public class ProducerProfile : Profile
+    {
+        public ProducerProfile()
+        {
+            CreateMap<ProducerDTO, Producer>()
+                .ReverseMap();
+        }
+    }
+}
