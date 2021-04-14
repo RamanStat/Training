2e709deeb934d6e00fd4a967667ea6eab8dd2a16
Statkeivch Roman <romanstat@mail.ru>
2021-04-14 18:02:17 +0300

    Changed ExcelProfile

--- Training.Service/Mapping/ExcelProfile.cs
+++ Training.Service/Mapping/ExcelProfile.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using ExcelDataReader;
 using Training.SDK.DTO;
 
 namespace Training.Service.Mapping
@@ -8,15 +7,15 @@ namespace Training.Service.Mapping
     {
         public ExcelProfile()
         {
-            CreateMap<IExcelDataReader, ExcelDTO>()
-                .ForMember(e => e.AutopartName, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.AUTOPARTNAME_COLUMN_OFFSET)))
-                .ForMember(e => e.AutopartPrice, source => source.MapFrom(s => double.Parse(s.GetValue(Constants.ImportFileOffsets.AUTOPARTPRICE_COLUMN_OFFSET).ToString())))
-                .ForMember(e => e.AutopartDescription, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.AUTOPARTDESCRIPTION_COLUMN_OFFSET)))
-                .ForMember(e => e.ProducerName, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.PRODUCERNAME_COLUMN_OFFSET)))
-                .ForMember(e => e.CarModel, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.CARMODEL_COLUMN_OFFSET)))
-                .ForMember(e => e.CarIssueYear, source => source.MapFrom(s => int.Parse(s.GetValue(Constants.ImportFileOffsets.CARISSUEYEAR_COLUMN_OFFSET).ToString())))
-                .ForMember(e => e.CarEngine, source => source.MapFrom(s => int.Parse(s.GetValue(Constants.ImportFileOffsets.CARENGINE_COLUMN_OFFSET).ToString())))
-                .ForMember(e => e.VendorName, source => source.MapFrom(s => s.GetValue(Constants.ImportFileOffsets.VENDORNAME_COLUMN_OFFSET)))
+            CreateMap<string[], ExcelDTO>()
+                .ForMember(e => e.AutopartName, source => source.MapFrom(s => s[Constants.ImportFileOffsets.AUTOPARTNAME_COLUMN_OFFSET]))
+                .ForMember(e => e.AutopartPrice, source => source.MapFrom(s => double.Parse(s[Constants.ImportFileOffsets.AUTOPARTPRICE_COLUMN_OFFSET])))
+                .ForMember(e => e.AutopartDescription, source => source.MapFrom(s => s[Constants.ImportFileOffsets.AUTOPARTDESCRIPTION_COLUMN_OFFSET]))
+                .ForMember(e => e.ProducerName, source => source.MapFrom(s => s[Constants.ImportFileOffsets.PRODUCERNAME_COLUMN_OFFSET]))
+                .ForMember(e => e.CarModel, source => source.MapFrom(s => s[Constants.ImportFileOffsets.CARMODEL_COLUMN_OFFSET]))
+                .ForMember(e => e.CarIssueYear, source => source.MapFrom(s => int.Parse(s[Constants.ImportFileOffsets.CARISSUEYEAR_COLUMN_OFFSET])))
+                .ForMember(e => e.CarEngine, source => source.MapFrom(s => int.Parse(s[Constants.ImportFileOffsets.CARENGINE_COLUMN_OFFSET])))
+                .ForMember(e => e.VendorName, source => source.MapFrom(s => s[Constants.ImportFileOffsets.VENDORNAME_COLUMN_OFFSET]))
                 .ReverseMap();
         }
     }
