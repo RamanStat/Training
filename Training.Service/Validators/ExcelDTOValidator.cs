b0cde50dc5a55bbacd54cb159c5d2c3a26d6783a
Statkeivch Roman <romanstat@mail.ru>
2021-04-14 19:29:56 +0300

    refactoring

--- Training.Service/Validators/ExcelDTOValidator.cs
+++ Training.Service/Validators/ExcelDTOValidator.cs
@@ -10,9 +10,9 @@ namespace Training.Service.Validators
         {
             RuleFor(e => e.AutopartName).NotEmpty().Length(3, 40);
             RuleFor(e => e.AutopartPrice).GreaterThan(0);
-            RuleFor(e => e.AutopartDescription).MinimumLength(20);
+            RuleFor(e => e.AutopartDescription).NotEmpty().MinimumLength(20);
             RuleFor(e => e.ProducerName).NotEmpty().Length(3, 40);
-            RuleFor(e => e.CarModel).Length(1, 25);
+            RuleFor(e => e.CarModel).NotEmpty().Length(1, 25);
             RuleFor(e => e.CarIssueYear).InclusiveBetween(1850, DateTime.Now.Year);
             RuleFor(e => e.CarEngine).InclusiveBetween(0, 2);
             RuleFor(e => e.VendorName).NotEmpty().Length(3, 40);
