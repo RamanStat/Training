f6bc3c9271c299aac9ba10011dc606415b5d30ab
Statkeivch Roman <romanstat@mail.ru>
2021-04-16 10:16:42 +0300

    Added ExceptionMiddleware

---
+++ Training.Service/Extensions/GlobalExceptionMiddleware.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Training.Service.Middleware;
+
+namespace Training.Service.Extensions
+{
+    public static class GlobalExceptionMiddleware
+    {
+        public static void UseGlobalExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
---
+++ Training.Service/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace Training.Service.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _request;
+
+        public ExceptionMiddleware(RequestDelegate request)
+        {
+            _request = request;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _request(context);
+            }
+            catch (Exception exception)
+            {
+                switch (exception)
+                {
+                    case var _ when exception is ValidationException:
+                        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        await context.Response.WriteAsJsonAsync(exception.Message);
+                        break;
+                    default:
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                        await context.Response.WriteAsJsonAsync(exception.Message);
+                        break;
+                }
+            }
+        }
+    }
+}
